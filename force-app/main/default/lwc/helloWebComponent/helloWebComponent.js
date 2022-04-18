f5984e190bf3d3b085128ce9b20b5d49846ff8dd
jGuayanay <jguayanay@omegacrmconsulting.com>
2022-04-18 12:44:52 +0200

    commitprimero

---
+++ force-app/main/default/lwc/helloWebComponent/helloWebComponent.js
@@ -0,0 +1,7 @@
+/**
+ * @description       : 
+ * @author            : Johnny A. Guayanay Troya
+ * @group             : 
+ * @last modified on  : 04-18-2022
+ * @last modified by  : Johnny A. Guayanay Troya
+**/
